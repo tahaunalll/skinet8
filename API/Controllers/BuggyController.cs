99d5a2e3acc4a740d5b78a7da17e2de3f7b46fa0
tahaunalll <tahaunal@uludag.edu.tr>
2024-08-13 21:58:13 +0300

    End of Section 6

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController:BaseApiController
+    {
+        [HttpGet("unauthorized")]
+        public IActionResult GetUnauthorized()
+        {
+            return Unauthorized(); 
+        }
+
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest("not a good request.."); 
+        }
+
+        [HttpGet("notfound")]
+        public IActionResult GetNotFound()
+        {
+            return NotFound(); 
+        }
+
+        [HttpGet("internalerror")]
+        public IActionResult GetInternalError()
+        {
+            throw new Exception ("This is a test exception");
+        }
+
+        [HttpPost("validationerror")]
+        public IActionResult GetValidationError (CreateProductDto product)
+        {
+            return Ok();
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -16,10 +17,16 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+//normalde http:5000 den https 5001 den dinleniyordu 
 
 app.MapControllers();
 
