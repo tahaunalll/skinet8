51f9f23958082029fb5c7bf51ce057af8efa8485
tahaunalll <tahaunal@uludag.edu.tr>
2024-09-02 15:58:07 +0300

    End of Section 11

--- API/DTOs/CreateProductDto.cs
+++ API/DTOs/CreateProductDto.cs
@@ -14,7 +14,7 @@ namespace API.DTOs
         [Required]
         public  string Description { get; set; } =string.Empty;
         
-        [Range(0.01, double.MaxValue, ErrorMessage ="Prica must be greater than 0")]
+        [Range(0.01, double.MaxValue, ErrorMessage ="Price must be greater than 0")]
         public decimal Price { get; set; }
 
         [Required]
