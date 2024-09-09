7f29027521d8d6d844f756ac06076e16e75f5781
tahaunalll <tahaunal@uludag.edu.tr>
2024-09-09 18:41:54 +0300

    End of Section 16

--- client/src/app/core/services/stripe.service.ts
+++ client/src/app/core/services/stripe.service.ts
@@ -1,10 +1,11 @@
 import { inject, Injectable } from '@angular/core';
-import { loadStripe, Stripe, StripeAddressElement, StripeAddressElementOptions, StripeElements } from '@stripe/stripe-js'
+import { ConfirmationToken, loadStripe, Stripe, StripeAddressElement, StripeAddressElementOptions, StripeElements, StripePaymentElement } from '@stripe/stripe-js'
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { CartService } from './cart.service';
 import { Cart } from '../../shared/models/cart';
 import { firstValueFrom, map } from 'rxjs';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,12 @@ import { firstValueFrom, map } from 'rxjs';
 export class StripeService {
   baseUrl =environment.apiUrl;
   private cartService = inject(CartService)
+  private accountService = inject(AccountService);
   private http =inject(HttpClient)
   private stripePromise: Promise<Stripe | null>
   private elements? : StripeElements;
   private addressElement?: StripeAddressElement;
+  private paymentElement?: StripePaymentElement;
 
 
   constructor() {
@@ -39,12 +42,43 @@ export class StripeService {
     return this.elements;
   }
 
+  async createPaymentElement(){
+    if(!this.paymentElement){
+      const elements = await this.initializeElements();
+      if(elements){
+        this.paymentElement = elements.create('payment');
+      } else {
+        throw new Error ('Elements instance has not been initializeed');
+      }
+    }
+    return this.paymentElement;
+  }
+
   async createAddressElement(){
     if(!this.addressElement){
       const elements = await this.initializeElements();
       if(elements){
+        const user = this.accountService.currentUser();
+        let defaultValues : StripeAddressElementOptions['defaultValues'] = {};
+
+        if(user){
+          defaultValues.name = user.firstName +' ' + user.lastName;
+        }
+
+        if(user?.address){
+          defaultValues.address = {
+            line1: user.address.line1,
+            line2: user.address.line2,
+            city: user.address.city,
+            state:user.address.state,
+            country:user.address.country,
+            postal_code : user.address.postalCode
+          }
+        }
+
         const options: StripeAddressElementOptions = {
-          mode:'shipping'
+          mode:'shipping',
+          defaultValues
         };
         this.addressElement = elements.create('address',options);
       } else {
@@ -55,16 +89,57 @@ export class StripeService {
   }
 
 
+async createConfirmationToken(){
+  const stripe = await this.getStripeInstance();
+  const elements = await this.initializeElements();
+  const result = await elements.submit();
+  if ( result.error) throw new Error(result.error.message);
+  if(stripe){
+    return await stripe.createConfirmationToken({elements});
+  } else{
+    throw new Error("Stripe not available");
+  }
+}
+
+async confirmPayment(confirmationToken : ConfirmationToken){
+  const stripe = await this.getStripeInstance();
+  const elements = await this.initializeElements();
+  const result = await elements.submit();
+  if ( result.error) throw new Error(result.error.message);
+
+  const clientSecret =  this.cartService.cart()?.clientSecret;
+
+  if(stripe && clientSecret){
+    return await stripe.confirmPayment({
+      clientSecret : clientSecret,
+      confirmParams: {
+        confirmation_token: confirmationToken.id
+      },
+      redirect : 'if_required'
+    })
+  }else{
+    throw new Error('Unable to load stripe');
+  }
+}
+
   createOrUpdatePaymentIntent(){
     const cart = this.cartService.cart();
     if(!cart) throw new Error('Problem with cart');
     return this.http.post<Cart>(this.baseUrl+'payments/'+ cart.id, {}).pipe(
       map(cart => {
-        this.cartService.cart.set(cart);
+        this.cartService.setCart(cart);
         return cart;
       })
     )
 
+
+  }
+
+  disposeElements(){
+    this.elements = undefined;
+    this.addressElement = undefined;
+    this.paymentElement = undefined;
+    
   }
 
 }
